0e95ba76dd92adb3ad51374fb732c7de1d475bee
David CLAIN <dclain@gmail.com>
2017-10-26 11:44:39 +0200

    Sortie en json et http + simplfication du code main

---
+++ README.md
@@ -0,0 +1,43 @@
+# Go Http socket proxy
+
+Ce projet *exemple* se base sur le code de [sparrc](https://github.com/sparrc/go-ping). Chaque appel à la commande ping réalise un ping unique, contrairement à celui de sparrc. Il appartient à l'appelant d'encapsuler son ping dans une go routine.
+
+Je l'utilise pour palier à des instabilités de la librairie 'net-ping' pour nodejs (En effet, 'raw-socket' sur lequel se base 'net-ping' s'installe directement dans la boucle principale. Dans certaines circonstances d'utilisation de 'net-ping', la boucle principale de nodejs se plantait lamentablement.).
+
+Il est prévu d'implémenter d'autres requêtes raw socket (UDP, TCP, ...) seulement si j'en ai besoin dans mes projets annexes (donc peut-être un jour :-/ ).
+
+## Comment ça marche ?
+* Lancer la commande 'socketproxy' ou 'socketproxy.exe' comme suit :
+    ```bash
+    ./socketproxy [-h localhost] [-p 9797] [-privileged]
+    #	-h to define host instead of 'localhost'
+    #	-p to define port superior to 1024 instead of '9797'
+    #	-privileged to activate privileged ping mode'
+    ```
+
+    Il est possible de l'embarquer dans un projet node comme suit :
+    ```javascript
+    // TODO
+    ```
+
+* socketproxy écoute par défaut sur le port 9797 et fournit les microservice suivants:
+    > http://localhost:9797/ping?timeout=2s&ip=192.168.XXX.XXX
+    >
+    > Paramètre timeout : respecter la syntaxe définie ici : https://golang.org/pkg/time/#ParseDuration
+    >
+    > Paramètr ip : l'ip vers lequel lancer un ping
+    >
+    > La valeur de retour :
+    ```json
+    {"result":true,"ip":"216.58.213.131","time":10069000}
+    ```
+    > Time s'exprime en nanosecondes
+    >
+    > La valeur de retour en cas d'erreur : 
+    ```json
+    {"result":false,"ip":"dfslk","time":0}
+    ```
+
+
+## A veiller !
+* Le repo principal n'est pas github. En récupérant les sources, assurez-vous donc de modifier le chemin du package vers pingutils.go dans le fichier cmd/main.go
---
+++ cmd/socketproxy/main.go
@@ -0,0 +1,115 @@
+package main
+
+// Go code 1.9
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+
+	"local-git.dcn.ovh/dclain/socketproxy"
+)
+
+func startHTTPServer(host string, port int) *http.Server {
+	srv := &http.Server{Addr: host + ":" + strconv.Itoa(port)}
+
+	http.HandleFunc("/ping", controllerPing)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("Httpserver: ListenAndServe() error: %s", err)
+		}
+	}()
+
+	return srv
+}
+
+var usage = `
+	-h defines host instead of 'localhost'
+	-p defines port superior to 1024 instead of '9797'
+	-privileged activates privileged ping mode'
+`
+var privileged = false
+
+func init() {
+	flag.Usage = func() {
+		fmt.Println(usage)
+		os.Exit(-1)
+	}
+}
+
+func main() {
+	host := flag.String("h", "localhost", "the host")
+	port := flag.Int("p", 9797, "the port")
+	priv := flag.Bool("privileged", false, "if ping is privileged or not")
+
+	flag.Parse()
+
+	if *host == "" || *port <= 1024 {
+		flag.Usage()
+	}
+
+	privileged = *priv
+
+	srv := startHTTPServer(*host, *port)
+
+	cout := make(chan os.Signal, 1)
+	signal.Notify(cout, os.Interrupt)
+	signal.Notify(cout, syscall.SIGTERM)
+
+	// attend une interruption
+	// plus efficace et moins consommateur de ressource que de faire un for / select
+	<-cout
+
+	if err := srv.Shutdown(nil); err != nil {
+		panic(err) // failure/timeout shutting down the server gracefully
+	}
+}
+
+type pingResult struct {
+	Result bool          `json:"result"`
+	IP     string        `json:"ip"`
+	Time   time.Duration `json:"time"`
+}
+
+// Ping handler control
+func controllerPing(w http.ResponseWriter, req *http.Request) {
+	if err := req.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ERR:%v", err)
+		return
+	}
+
+	timeout, err := time.ParseDuration(req.Form.Get("timeout"))
+	if err != nil {
+		fmt.Fprintf(w, "ERR:%v", err)
+		return
+	}
+
+	packet := socketproxy.PingUtil(timeout, privileged, req.Form.Get("ip"))
+	// sends jsons objet by default
+
+	result := &pingResult{Result: false, IP: req.Form.Get("ip"), Time: 0}
+	if packet != nil {
+		result.IP = packet.IPAddr.IP.String()
+		result.Result = true
+		result.Time = packet.Rtt
+		log.Println(packet)
+	}
+
+	json, err := json.Marshal(result)
+	if err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintf(w, "KO:%q", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "%s", json)
+}
